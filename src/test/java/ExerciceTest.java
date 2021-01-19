af121689ca4e7520007499a851ab1cc723bef0bd
lang <hoanglang@gmail.com>
2021-01-19 23:40:07 +0100

    Test data

--- src/test/java/ExerciceTest.java
+++ src/test/java/ExerciceTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static java.util.stream.Collectors.toList;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ExerciceTest {
@@ -10,5 +10,63 @@ class ExerciceTest {
     @DisplayName("This is what a test looks like")
     void example() {
         assertThat("hello".length()).isEqualTo(5);
+    }
+
+    @Test
+    void test1() {
+        var input = List.of("50 +", "52 -");
+
+
+        assertThat("").isEqualTo("51");
+    }
+
+    @Test
+    void test2() {
+        var input = List.of(
+                "25 +",
+                "35 +",
+                "99 -",
+                "90 -",
+                "38 ="
+        );
+
+
+        assertThat("").isEqualTo("38");
+    }
+
+    @Test
+    void test3() {
+        var input = List.of(
+                "37 -",
+                "28 +",
+                "51 -",
+                "62 +",
+                "25 -"
+        );
+
+
+        assertThat("").isEqualTo("Impossible");
+    }
+
+    @Test
+    void test4() {
+        var input = List.of(
+                "62 -",
+                "38 +",
+                "72 -",
+                "40 -",
+                "32 ="
+        );
+
+
+        assertThat("").isEqualTo("Impossible");
+    }
+
+    @Test
+    void test5() {
+        var input = List.of("99 +");
+
+
+        assertThat("").isEqualTo("Impossible");
     }
 }
